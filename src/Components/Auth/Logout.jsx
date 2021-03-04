7f6c9dd36ed95e843dafb13e4a57a4e98fbde428
upify-tech <78359832+upify-tech@users.noreply.github.com>
2021-03-04 15:21:26 +0530

    Delete src/Components directory

--- src/Components/Auth/Logout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-
-const Logout = () => {
-    localStorage.removeItem("token");
-      return <Redirect to="/signup" />
-}
-
-export default Logout;
